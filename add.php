efd77b9e151f23e51f09abbf8c202420f8c73179
YousefDev1 <71141247+YousefDev1@users.noreply.github.com>
2020-12-14 00:15:31 +0200

    Update add.php

--- add.php
+++ add.php
@@ -9,43 +9,53 @@
 <body>
     <?php
 
+        //Include files in the page
         require "conn.php";
         include "tmbl/header.php";
+
+        //Set date value
         $date = Date('Y/n/j');
 
+        //Variables
         $title   = '';
         $author  = '';
         $content = '';
 
+        //Set title
         if(isset($_POST['title']))
         {
             $title = $_POST['title'];
         }
 
+        //Set author name
         if(isset($_POST['author']))
         {
             $author = $_POST['author'];
         }
 
+        //Set content
         if(isset($_POST['content']))
         {
             $content = $_POST['content'];
         }
 
+        //Button Click and send data
         if(isset($_POST['post']))
         {
+            //Send data to database
             mysqli_query($db_conn, "INSERT INTO posts(
                 `title`, `author`, `content`, `date`
             ) VALUES(
                 '$title', '$author', '$content', '$date'
             )");
 
+            //Set header location at index.php
             header('location: index.php');
         }
 
     ?>
 
-    
+   
     <div class="add-container">
         <form action="" method="POST" class="add">
             <h1>Add Post</h1>
